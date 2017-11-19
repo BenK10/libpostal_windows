0897a132b02560ce6d84e8b5727e22025f0e6321
Benjamin Kusin <BenK10@users.noreply.github.com>
2017-11-18 20:59:27 -0500

    Delete tagger.h

--- src/tagger.h
+++
@@ -1,5 +0,0 @@
-#include "string_utils.h"
-#include "tokens.h"
-
-// Arguments:                           tagger, context, tokenized str,       index
-typedef bool (*tagger_feature_function)(void *, void *, tokenized_string_t *, uint32_t);
